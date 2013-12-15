7efc3f9f20950b07a612638f6a006684baf5fdfc
Vincentoo <mark.olenik@gmail.com>
2013-12-15 13:39:56 +0000

    write_to_log.m: changed timestamp format

--- write_to_log.m
+++ write_to_log.m
@@ -1,28 +1,25 @@
-function [] = write_to_log( session_folder, log_event )
-%WRITE_TO_LOG appends timestamped event to logfile. Creates a new log file,
-%   if not existent.
+function [] = write_to_log( log_path, log_event )
+%WRITE_TO_LOG appends timestamped event to logfile. Creates or modifies log
 %
 % SYNOPSIS
 %   [] = write_to_log( session_folder, log_event )
 %
 % INPUT
-%   (string)  session_folder: folder that contains the parameter file (as .mat) and
-%                             all EEG files (rest1, pre_stim, post_stim,
-%                             rest2 at .dat)
+%   (string)  log_path: path to log folder in session folder
 %   (string)  log_event: event to written to logfile
 
-log_file  = [session_folder, filesep, 'log.txt'];
+log_file  = [log_path, filesep, 'log.txt'];
 
 % timestamp up to seconds precision
-timestamp = datestr(clock,'yyyy-mm-dd-HH:MM:SS');
+timestamp = datestr(clock,'yyyy-mm-dd|HH:MM:SS');
 % create log.txt
 fid = fopen(log_file, 'at+');
 [ST,~] = dbstack;
-fprintf(fid, '%s\n', [timestamp, ': "', ST(2).name, '" called']);
+% write function that writes event
+fprintf(fid, '%s\n', [timestamp, ': "', ST(2).name, '.m" called']);
+% write event
 fprintf(fid, '%s\n', [timestamp, ': ', log_event]);
 fclose(fid);
-
-%some change
 
 end
 

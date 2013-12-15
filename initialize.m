506e33852c4db0a8a3b71d9781be718fffc002f0
Vincentoo <mark.olenik@gmail.com>
2013-12-15 22:19:19 +0000

    initialize.m: added log_path output.

--- initialize.m
+++ initialize.m
@@ -1,11 +1,12 @@
-function [params, rest1_eeg, pre_stim_eeg, post_dur_stim_eeg, rest2_eeg] = initialize(session_path)
+function [params, rest1_eeg, pre_stim_eeg, post_dur_stim_eeg, rest2_eeg,...
+    log_path] = initialize(session_path)
 %INITIALIZE Initializes analysis of a session.
 % Return params and eeg file-paths when done succesfully. Additionally, a
 % log folder is created. It will contain all steps of processed data
 % (filering etc.).
 %
 % SYNOPSIS
-%   [params, rest1_eeg, pre_stim_eeg, post_dur_stim_eeg, rest2_eeg] = initialize(session_folder)
+%   [params, rest1_eeg, pre_stim_eeg, post_dur_stim_eeg, rest2_eeg log_path] = initialize(session_folder)
 %
 % INPUT
 %   (string)  session_path: path to folder that contains the parameter file (as .mat) and
@@ -13,17 +14,19 @@ function [params, rest1_eeg, pre_stim_eeg, post_dur_stim_eeg, rest2_eeg] = initi
 %                             rest2 at .dat)
 %
 % OUPUT
+%TODO: add path again to variable names
 %   (string)  params:               path to params file
 %   (string)  rest1_eeg:            path to rest 1 EEG file
 %   (string)  pre_stim_eeg:         path to pre-stim EEG file
 %   (string)  post_dur_stim_eeg:    path to post/during-stim EEG file
 %   (string)  rest2_eeg:            path to rest 2 EEG file
+%   (string)  log_path              path to logfile
 
 % get subject ID
 folder      = session_path(strfind(session_path, 'subject_'):end);
 localize    = strfind(folder,'_');
-subject = folder(localize(1)+1:localize(2)-1);
-
+subject     = folder(localize(1)+1:localize(2)-1);
+log_path    = [session_path filesep 'log'];
 
 condition   = '';
 % find out what session we are looking at, by looking for _<condition> in
@@ -42,7 +45,7 @@ end
 
 % the following three files are essential to analyse the session. their
 % filenames consist of subject and condition as follows
-params         = [session_path filesep 'subject' '_' subject,'_',condition,'.mat'];
+params              = [session_path filesep 'subject' '_' subject,'_',condition,'.mat'];
 rest1_eeg           = [session_path filesep 'subject' '_' subject,'_',condition,'S001R01.dat'];
 pre_stim_eeg        = [session_path filesep 'subject' '_' subject,'_',condition,'S001R02.dat'];
 post_dur_stim_eeg   = [session_path filesep 'subject' '_' subject,'_',condition,'S001R03.dat'];
@@ -53,11 +56,11 @@ if(exist(params, 'file') && exist(pre_stim_eeg, 'file') && ...
         exist(post_dur_stim_eeg, 'file') && exist(rest1_eeg, 'file') ...
         && exist(rest2_eeg, 'file'))
     % create log folder
-    mkdir([session_path, filesep, 'log']);
-    write_to_log([session_path filesep 'log'], ['successfully initialized session ', '"', condition, '"', ' for subject ', '"', subject, '"']);
+    mkdir(log_path);
+    write_to_log(log_path, ['successfully initialized session ', '"', condition, '"', ' for subject ', '"', subject, '"']);
     success = true;
 else
-    write_to_log([session_path filesep 'log'], ['files missing in session ', '"', condition, '"', ' for subject ', '"', subject, '"']);
+    write_to_log(log_path, ['files missing in session ', '"', condition, '"', ' for subject ', '"', subject, '"']);
     success = false;
 
 % now that everything is in place, the preprocessing can begin

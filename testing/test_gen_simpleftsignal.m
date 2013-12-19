5a594db261ee637b0226bc0407a711acfe5fb085
Vincentoo <mark.olenik@gmail.com>
2013-12-19 14:42:21 +0000

    gen_simleftsignal.m: seems to work

--- testing/test_gen_simpleftsignal.m
+++ testing/test_gen_simpleftsignal.m
@@ -6,9 +6,19 @@ addpath('/Users/mark/Dropbox/tACS_project_matlab_code/fieldtrip-20130901');
 addpath(genpath('/Users/mark/Dropbox/tacs_project_matlab_code/functions'));
 ft_defaults;
 
-fsample = 1000;
-trllength = 1000;
-numtrl = 20;
+
+session_path = '/Users/mark/Dropbox/tACS_project_matlab_code/exampleData/subject_BL_desynch001';
+
+[params, rest1_eeg, pre_stim_eeg, post_dur_stim_eeg, rest2_eeg, log_path] =...
+    initialize(session_path);
+load(params);
+
+%%
+% testing on pre-stim data
+
+fsample = params.Recording.SamplingRate;
+trllength = params.Task.Trial(params.Task.TrialCount.PreStimTrials).Timing.TrialEnd; % in s
+numtrl = 1; % continuous data
 
 [simplesignal] = gen_simpleftsignal(fsample,trllength,...
     numtrl);
